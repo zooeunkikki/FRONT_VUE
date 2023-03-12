566b5cba9449ea89dd06d86d10c0171a42a8df02
juenkim <rlawndms517@gmail.com>
2023-03-12 13:46:44 +0900

    학원에서 진행했던 파일을 넣기

---
+++ LectureMaterial/ES2015문법/ap06.ES2015문법-cli/src/Module3.js
@@ -0,0 +1,14 @@
+/*
+  CommonJS 모듈 방식 : require 사용
+    var module = require('./MyModule');
+
+
+  ES2015 모듈 방식 : import/export 사용
+    import React from "redux";
+    import {Component} from "redux";
+    import React, {Component} from "redux";
+    import React as rd from "redux";
+    import * as rd from "redux";
+*/
+
+// Module3.js
